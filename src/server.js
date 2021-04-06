3d280a9604cd27de0c315e0fcf983d9548ec2f02
Sofia K <sofiakovalevskaia@gmail.com>
2021-04-06 11:41:13 +0100

    created get routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 
+// always indicate the function with (request, response) => {}
+app.get("/", (req, res) => {
+    // any code is valid here
+    console.log("/ endpoint has been hit");
+    // last thing we do is a send response
+    res.send("Hello Kov");
+});
+
+// created a new get route named "kov" with a response containing an object with my name and age 
+app.get("/kov", (req,res) => {
+    let person = {
+        fName: "Sofia",
+        lName: "Kovalevskaia",
+        age: 27,
+    };
+    res.send({person});
+});
+
+// created another get route named "kovother" to see which style of code I prefer for objects 
+app.get("/kovother", (req,res) => {
+    res.send({person: {fName: "Sofia", lName: "Kovalevskaia", age: 27}});
+});
+
 app.listen(5000, () => {
     console.log("listening on port 5000");
 });
